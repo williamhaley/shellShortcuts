1d9b62fcbc5fd8a5a13438b54419fb588b3407ea
Will <willhy@gmail.com>
2017-10-31 17:08:31 -0500

    Clean up shell scripts. Remove unused.

--- shell/raspberry_pi.bash
+++
@@ -1,3 +0,0 @@
-alias jack_output='sudo modprobe snd_bcm2835 && amixer cset numid=3 1'
-alias hdmi_output='sudo modprobe snd_bcm2835 && amixer cset numid=3 0'
-alias video='mplayer -nolirc -vo fbdev2:/dev/fb0 $@'
