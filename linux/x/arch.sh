83eb4cfd5a910a99658fc0ea504aed672a726fec
Will <willhy@gmail.com>
2018-03-17 09:16:45 -0500

    Update x config

--- linux/x/arch.sh
+++ linux/x/arch.sh
@@ -2,21 +2,9 @@
 
 [ $EUID -ne 0 ] && echo "run as root" >&2 && exit 1
 
+# If you want a full X WM, just install Mate
+
 pacman -Syy --noconfirm --needed \
-	xorg xorg-server \
-	lightdm lightdm-gtk-greeter gnome-keyring \
+	xorg xorg-server gnome-keyring openbox \
 	noto-fonts noto-fonts-emoji ttf-dejavu
-
-pacman -Syy --noconfirm \
-	mate mate-extra
-
-pacman -Syy --noconfirm \
-	wicd wicd-gtk
-
-# wicd auto-start behavior is weird. Only launch
-# if manually if needed. Use a script/cli for wpa_supplicant
-# otherwise.
-rm /etc/xdg/autostart/wicd-tray.desktop
-
-systemctl enable lightdm
 
