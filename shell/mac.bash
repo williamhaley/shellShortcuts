fe2550c7f7cb92b750c213da3096b075cf22a7b1
Will <willhy@gmail.com>
2018-04-18 19:15:55 -0500

    Remove old aliases

--- shell/mac.bash
+++ shell/mac.bash
@@ -1,23 +1,12 @@
 # If not running interactively, don't do anything
 [[ $- != *i* ]] && return
 
+# Enable colors.
 export CLICOLOR=1
 # Light background.
 #export LSCOLORS=ExFxCxDxBxegedabagacad
 # Dark background.
 export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
-
-PATH=$PATH:~/'Android Studio.app'/sdk/platform-tools
-
-case "$TERM" in
-    screen*) PROMPT_COMMAND='echo -ne "\033k\033\0134"'
-esac
-
-alias logcat-color='adb logcat | grep -i `adb shell ps | grep -i 'com.homefinder' | cut -c10-15` | logcat-color'
-alias setJdk6='export JAVA_HOME=$(/usr/libexec/java_home -v 1.6)'
-alias setJdk7='export JAVA_HOME=$(/usr/libexec/java_home -v 1.7)'
-alias setJdk8='export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)'
-alias simulators='xcrun simctl list'
 
 export IS_MAC=true
 
