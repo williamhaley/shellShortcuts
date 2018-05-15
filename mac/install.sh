113be3b3cf82698f26f80c19f6f7dc7a316bace3
Will <willhy@gmail.com>
2018-05-15 13:24:28 -0500

    Clean up mac install script

--- mac/install.sh
+++ mac/install.sh
@@ -2,15 +2,12 @@
 
 brew install gpg
 brew cask install 1password
-brew cask install vnc-viewer
 brew install cdrtools
 brew install rclone
 brew cask install caffeine
-brew cask install licecap
 brew cask install gimp
 brew cask install iterm2
 brew cask install google-chrome
-brew cask install atom
 brew install Caskroom/cask/virtualbox
 brew install Caskroom/cask/vagrant
 brew cask install google-drive
@@ -18,10 +15,7 @@ brew cask install vlc
 brew install homebrew/dupes/rsync
 brew install qemu
 brew cask install slack
-brew install Caskroom/versions/intellij-idea-ce
 brew cask install eclipse-java
-brew cask install spotify
-brew install Caskroom/cask/dropbox
 brew cask install keepassx # https://www.keepassx.org/downloads/
 brew cask install google-drive
 brew cask install java
