b62561c2b595e725c3ff281f7dea55ccc943c7d8
Will <willhy@gmail.com>
2018-07-03 19:06:46 -0500

    Add a simple bash prompt. No colors

---
+++ shell/prompt.simple.bash
@@ -0,0 +1,15 @@
+source $CONFIGS_DIR/shell/git-prompt.sh
+
+GIT_PS1_SHOWDIRTYSTATE=true
+GIT_PS1_SHOWSTASHSTATE=true
+GIT_PS1_SHOWUNTRACKEDFILES=true
+GIT_PS1_SHOWUPSTREAM="auto"
+GIT_PS1_STATESEPARATOR=" "
+
+PS1=""
+
+PS1+='\h \u \w $(__git_ps1 "[%s]")'
+PS1+="\n$ "
+
+PROMPT_COMMAND="echo"
+
