d14b0a2d0a63311a19b1c59b415b257cd4db3f24
Will <willhy@gmail.com>
2019-01-14 20:40:29 -0600

    Simplify PS1

--- shell/prompt.bash
+++ shell/prompt.bash
@@ -35,71 +35,8 @@ GIT_PS1_SHOWUNTRACKEDFILES=true
 GIT_PS1_SHOWUPSTREAM="auto"
 GIT_PS1_STATESEPARATOR=" "
 
-system-emoji()
-{
-	EMOJIS=(🐶 ⚽️ 🏀 🏈 🎾 ⛵ 🐷 🐧 🍀 🌴 🌊)
-	HOSTNAME_HASH=$(for c in $(grep -o . <<<`hostname`); do printf '%d' "'$c"; done)
-	HOSTNAME_SHORT_HASH=$(printf ${HOSTNAME_HASH} | cut -b 1-5)
-
-	NUM_FROM_HASH=$(printf $HOSTNAME_HASH | printf $((16#${HOSTNAME_SHORT_HASH})))
-	LEN=${#EMOJIS[@]}
-
-	INDEX=$(expr ${NUM_FROM_HASH} % ${LEN})
-	printf ${EMOJIS[INDEX]}
-}
-
-__version()
-{
-	label=$1
-	bin=$2
-	regex=$3
-	versionCmd=$4
-	tputColor=$5
-	useSeparator=$6
-
-	if ! $(test -x $(which "$bin" 2> /dev/null | head -1)) 2>/dev/null;
-	then
-		return 1
-	fi
-
-	location="sys"
-	if [[ "$(which "$bin" 2> /dev/null | head -1)" =~ "$HOME" ]];
-	then
-		location="user"
-	fi
-
-	if [[ ! "$(eval "$versionCmd" 2> /dev/null)" =~ $regex ]];
-	then
-		return 1
-	fi
-
-	local v="${BASH_REMATCH[1]}"
-	if [ $useSeparator = 0 ];
-	then
-		printf " | "
-	fi
-	printf "\001$(tput setaf $tputColor)\002${label} [$location]:\001$(tput sgr0)\002 ${v}"
-}
-
-function __versions()
-{
-	# Use return status to indicate whether or not we prepend a
-	# separator for each version number. 0 means separator, 1 means
-	# no separator.
-	local sep=1
-
-	__version "Go" "go" "go([0-9]+.[0-9]+[.[0-9]+]?)" "go version" 6 $sep && sep=0
-	__version "Python" "python" "([0-9]+.[0-9]+.[0-9]+)" "python --version" 3 $sep && sep=0
-	__version "Node" "node" "([0-9]+.[0-9]+.[0-9]+)" "node -v" 2 $sep && sep=0
-	__version "Ruby" "ruby" "([0-9]+.[0-9]+.[0-9]+)" "ruby -v" 1 $sep && sep=0
-}
-
 PS1="\n"
-PS1+="${CYAN_FG}\u${RESET} on ${CYAN_FG}\h${RESET} at ${CYAN_FG}\w${RESET} ${GREEN_FG}"'$(__git_ps1 "[%s]")'"${RESET}"
-PS1+="\n"'$(__versions)'"\n"
+PS1+="${CYAN_FG}\u${RESET} on ${CYAN_FG}\h${RESET} at ${CYAN_FG}\w${RESET} ${GREEN_FG}"'$(__git_ps1 "[%s] ")'"${RESET}"
 PS1+="${RED_FG}\$"
 PS1+="${RESET} "
-
-# Runs after every command.
-PROMPT_COMMAND="echo"
 
