fc55b80e26245368580a2798a8e1eae731b0eb74
Will <willhy@gmail.com>
2020-04-25 19:52:27 -0500

    Swap in geany for gedit

--- linux/configure.sh
+++ linux/configure.sh
@@ -193,7 +193,7 @@ _apps()
 		sudo openssh \
 		firefox chromium \
 		ntfs-3g exfat-utils mtools syslinux \
-		zsh vim gedit base-devel git linux-headers go docker docker-compose \
+		zsh vim geany base-devel git linux-headers go docker docker-compose \
 		keepassxc \
 		wget curl rclone rsync unzip \
 		net-tools tcpdump wireshark-cli nmap \
